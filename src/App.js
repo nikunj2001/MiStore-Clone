bbe025fc2e6a88882dbcd1acf92258f4a8e0cb90
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2021-12-10 22:01:07 +0530

    Final Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Nav from './components/Nav';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider.js';
+import Offers from './components/Offers.js';
+import Heading from './components/Heading';
+import StarProduct from './components/StarProduct.js';
+import HotAccessoriesMenu from './components/HotAccessoriesMenu.js';
+import ProductReviews from './components/ProductReviews.js';
+import HotAccessories from './components/HotAccessories';
+import Videos from './components/Videos.js';
+import Banner from './components/Banner.js';
+import Footer from './components/Footer.js';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom"
+import data from "./data/data.json"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Nav/>
+        <Navbar/>
+        <Slider start={data.banner.start} />
+        <Offers offer={data.offer}/>
+        <Heading text="STAR PRODUCTS" />
+        <StarProduct starProduct={data.starProduct} />
+        <Heading text="HOT ACCESSORIES"/>
+        <HotAccessoriesMenu/>
+        <Switch>
+        <Route path="/music" exact    >
+          <HotAccessories  music={data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music} />
+          </Route>
+        <Route path="/smartDevice" exact>
+          <HotAccessories  smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />
+          </Route>
+          
+       <Route exact path="/home">
+       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
+       </Route>
+
+       <Route exact path="/lifestyle">
+       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
+       </Route>
+
+
+       <Route exact path="/mobileAccessories">
+       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
+       </Route>
+
+      
+        </Switch>
+         <Heading  text="PRODUCT REVIEWS" />
+       <ProductReviews productReviews={data.productReviews}  />
+        <Heading text="VIDEOS" />
+        <Videos videos={data.videos} />
+        <Banner banner={data.banner} />
+        <Footer footer={data.footer} />
+      </>
+          
+    </Router>
   );
 }
-
 export default App;
